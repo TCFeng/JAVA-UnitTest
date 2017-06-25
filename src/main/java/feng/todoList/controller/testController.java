c560bc486fc363af2f368ed65866a4a63bcc277f
TCFeng <tcfeng601@gmail.com>
2017-06-25 20:55:51 +0800

    Init project

---
+++ src/main/java/feng/todoList/controller/testController.java
@@ -0,0 +1,14 @@
+package feng.todoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class testController {
+
+    @RequestMapping(value = {"/", "/test"}, method = RequestMethod.GET)
+    public String testMethod() {
+        return "OK!";
+    }
+}
