7a35e61037eaff2682df2492cdb293ba664ed5cf
TCFeng <tcfeng601@gmail.com>
2017-06-27 00:31:15 +0800

    Modify spring mvc xml

--- src/main/java/feng/todoList/controller/testController.java
+++ src/main/java/feng/todoList/controller/testController.java
@@ -1,14 +1,19 @@
 package feng.todoList.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/test")
 public class testController {
 
-    @RequestMapping(value = {"/", "/test"}, method = RequestMethod.GET)
-    public String testMethod() {
-        return "OK!";
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public String showHelloWorld(){
+
+        return "OK";
     }
 }
